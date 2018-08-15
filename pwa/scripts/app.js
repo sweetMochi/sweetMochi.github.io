d237528613ac2b74c5b26afcce959a66302ec662
Mochi <thepapermpty@gmail.com>
2018-08-15 10:17:54 +0800

    add localstorge

--- pwa/scripts/app.js
+++ pwa/scripts/app.js
@@ -27,6 +27,46 @@
 		daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 	};
 
+	/*
+	 * Fake weather data that is presented when the user first uses the app,
+	 * or when the user has not saved any cities. See startup code for more
+	 * discussion.
+	 */
+	var initialWeatherForecast = {
+		key: '2459115',
+		label: 'New York, NY',
+		created: '2016-07-22T01:00:00Z',
+		channel: {
+			astronomy: {
+				sunrise: "5:43 am",
+				sunset: "8:21 pm"
+			},
+			item: {
+				condition: {
+					text: "Windy",
+					date: "Thu, 21 Jul 2016 09:00 PM EDT",
+					temp: 56,
+					code: 24
+				},
+				forecast: [
+					{code: 44, high: 86, low: 70},
+					{code: 44, high: 94, low: 73},
+					{code: 4, high: 95, low: 78},
+					{code: 24, high: 75, low: 89},
+					{code: 24, high: 89, low: 77},
+					{code: 44, high: 92, low: 79},
+					{code: 44, high: 89, low: 77}
+				]
+			},
+			atmosphere: {
+				humidity: 56
+			},
+			wind: {
+				speed: 25,
+				direction: 195
+			}
+		}
+	};
 
 	/*****************************************************************************
 	 *
@@ -54,6 +94,8 @@
 		app.getForecast(key, label);
 		// TODO push the selected city to the array and save here
 		app.toggleAddDialog(false);
+		app.selectedCities.push({"key" : key, "label" : label})
+		app.saveSelectedCities()
 	});
 
 	document.getElementById('butAddCancel').addEventListener('click', function() {
@@ -61,12 +103,75 @@
 		app.toggleAddDialog(false);
 	});
 
-
 	/*****************************************************************************
 	 *
 	 * Methods to update/refresh the UI
 	 *
 	 ****************************************************************************/
+
+	app.getIconClass = function(weatherCode) {
+		// Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
+		weatherCode = parseInt(weatherCode);
+		switch (weatherCode) {
+			case 25: // cold
+			case 32: // sunny
+			case 33: // fair (night)
+			case 34: // fair (day)
+			case 36: // hot
+			case 3200: // not available
+				return 'clear-day';
+			case 0: // tornado
+			case 1: // tropical storm
+			case 2: // hurricane
+			case 6: // mixed rain and sleet
+			case 8: // freezing drizzle
+			case 9: // drizzle
+			case 10: // freezing rain
+			case 11: // showers
+			case 12: // showers
+			case 17: // hail
+			case 35: // mixed rain and hail
+			case 40: // scattered showers
+				return 'rain';
+			case 3: // severe thunderstorms
+			case 4: // thunderstorms
+			case 37: // isolated thunderstorms
+			case 38: // scattered thunderstorms
+			case 39: // scattered thunderstorms (not a typo)
+			case 45: // thundershowers
+			case 47: // isolated thundershowers
+				return 'thunderstorms';
+			case 5: // mixed rain and snow
+			case 7: // mixed snow and sleet
+			case 13: // snow flurries
+			case 14: // light snow showers
+			case 16: // snow
+			case 18: // sleet
+			case 41: // heavy snow
+			case 42: // scattered snow showers
+			case 43: // heavy snow
+			case 46: // snow showers
+				return 'snow';
+			case 15: // blowing snow
+			case 19: // dust
+			case 20: // foggy
+			case 21: // haze
+			case 22: // smoky
+				return 'fog';
+			case 24: // windy
+			case 23: // blustery
+				return 'windy';
+			case 26: // cloudy
+			case 27: // mostly cloudy (night)
+			case 28: // mostly cloudy (day)
+			case 31: // clear (night)
+				return 'cloudy';
+			case 29: // partly cloudy (night)
+			case 30: // partly cloudy (day)
+			case 44: // partly cloudy
+				return 'partly-cloudy-day';
+		}
+	};
 
 	// Toggles the visibility of the add new city dialog.
 	app.toggleAddDialog = function(visible) {
@@ -214,111 +319,28 @@
 	};
 
 	// TODO add saveSelectedCities function here
-
-	app.getIconClass = function(weatherCode) {
-		// Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
-		weatherCode = parseInt(weatherCode);
-		switch (weatherCode) {
-			case 25: // cold
-			case 32: // sunny
-			case 33: // fair (night)
-			case 34: // fair (day)
-			case 36: // hot
-			case 3200: // not available
-				return 'clear-day';
-			case 0: // tornado
-			case 1: // tropical storm
-			case 2: // hurricane
-			case 6: // mixed rain and sleet
-			case 8: // freezing drizzle
-			case 9: // drizzle
-			case 10: // freezing rain
-			case 11: // showers
-			case 12: // showers
-			case 17: // hail
-			case 35: // mixed rain and hail
-			case 40: // scattered showers
-				return 'rain';
-			case 3: // severe thunderstorms
-			case 4: // thunderstorms
-			case 37: // isolated thunderstorms
-			case 38: // scattered thunderstorms
-			case 39: // scattered thunderstorms (not a typo)
-			case 45: // thundershowers
-			case 47: // isolated thundershowers
-				return 'thunderstorms';
-			case 5: // mixed rain and snow
-			case 7: // mixed snow and sleet
-			case 13: // snow flurries
-			case 14: // light snow showers
-			case 16: // snow
-			case 18: // sleet
-			case 41: // heavy snow
-			case 42: // scattered snow showers
-			case 43: // heavy snow
-			case 46: // snow showers
-				return 'snow';
-			case 15: // blowing snow
-			case 19: // dust
-			case 20: // foggy
-			case 21: // haze
-			case 22: // smoky
-				return 'fog';
-			case 24: // windy
-			case 23: // blustery
-				return 'windy';
-			case 26: // cloudy
-			case 27: // mostly cloudy (night)
-			case 28: // mostly cloudy (day)
-			case 31: // clear (night)
-				return 'cloudy';
-			case 29: // partly cloudy (night)
-			case 30: // partly cloudy (day)
-			case 44: // partly cloudy
-				return 'partly-cloudy-day';
-		}
+	// Save list of cities to localStorage, see note below about localStorage.
+	app.saveSelectedCities = function() {
+		var selectedCities = JSON.stringify(app.selectedCities);
+		// IMPORTANT: See notes about use of localStorage.
+		localStorage.selectedCities = selectedCities;
 	};
 
-	/*
-	 * Fake weather data that is presented when the user first uses the app,
-	 * or when the user has not saved any cities. See startup code for more
-	 * discussion.
-	 */
-	var initialWeatherForecast = {
-		key: '2459115',
-		label: 'New York, NY',
-		created: '2016-07-22T01:00:00Z',
-		channel: {
-			astronomy: {
-				sunrise: "5:43 am",
-				sunset: "8:21 pm"
-			},
-			item: {
-				condition: {
-					text: "Windy",
-					date: "Thu, 21 Jul 2016 09:00 PM EDT",
-					temp: 56,
-					code: 24
-				},
-				forecast: [
-					{code: 44, high: 86, low: 70},
-					{code: 44, high: 94, low: 73},
-					{code: 4, high: 95, low: 78},
-					{code: 24, high: 75, low: 89},
-					{code: 24, high: 89, low: 77},
-					{code: 44, high: 92, low: 79},
-					{code: 44, high: 89, low: 77}
-				]
-			},
-			atmosphere: {
-				humidity: 56
-			},
-			wind: {
-				speed: 25,
-				direction: 195
-			}
-		}
-	};
+	app.selectedCities = localStorage.selectedCities;
+
+	if ( app.selectedCities ) {
+		app.selectedCities = JSON.parse(app.selectedCities);
+		app.selectedCities.forEach(function(city) {
+			app.getForecast(city.key, city.label);
+		});
+	} else {
+		app.updateForecastCard(initialWeatherForecast);
+		app.selectedCities = [
+			{key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+		];
+		app.saveSelectedCities();
+	}
+
 	// TODO uncomment line below to test app with fake data
 	app.updateForecastCard(initialWeatherForecast);
 
@@ -330,7 +352,5 @@
 			.register('/service-worker.js')
 			.then(function() { console.log('Service Worker Registered'); });
 	}
-
-
 
 })();
